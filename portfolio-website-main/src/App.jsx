7af86187113966f3e1019f794f12c3c5e69ddf7f
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-18 17:44:22 +0530

    Added Skills

--- portfolio-website-main/src/App.jsx
+++ portfolio-website-main/src/App.jsx
@@ -2,15 +2,17 @@ import About from "./Components/About/About";
 import Experience from "./Components/Experience/Experience";
 import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
+import Skills from "./Components/Skills/Skills";
 import Navbar from "./Components/Navbar/Navbar";
 import Projects from "./Components/Projects/Projects";
 
 function App() {
   return (
-    <div className="bg-[#171d32] h-auto w-full overflow-hidden">
+    <div className="bg-[#182452] h-auto w-full overflow-hidden">
       <Navbar />
       <Home />
       <About />
+      <Skills />
       <Experience />
       <Projects />
       <Footer />
